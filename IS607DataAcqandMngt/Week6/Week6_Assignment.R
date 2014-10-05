a06a9f8d005f3163cf99584e128fabf53de9aaa2
Shipra <shipraahuja@gmail.com>
2014-10-05 15:21:29 -0400

    Adding R script for Week6 Assignment

--- IS607DataAcqandMngt/Week6/Week6_Assignment.R
+++ IS607DataAcqandMngt/Week6/Week6_Assignment.R
@@ -10,22 +10,24 @@ phone <- html_nodes(html,".CommunityPhoneTextBold")
 phoneno <- html_text(phone)
 print (phoneno)
 
-## Pull out the rental office address of the apartment complex
-
-officeaddress <- html_nodes(html,"#ctl00_ContentPlaceHolder1_panPhoneoffHours tr:nth-child(6) td")
-addr <-html_text(officeaddress)
-print (addr)
-
 ## Pull out the business hours of the rental office of the apartment complex
 
 businesshrs <- html_nodes(html,"tr:nth-child(4) .CommunityOffHoursDetailsText")
 hours <- html_text(businesshrs)
 print (hours)
 
+## Pull out the rental office address of the apartment complex
+
+officeaddress <- html_nodes(html,"tr:nth-child(6) .CommunityOffHoursDetailsText")
+addr <-html_text(officeaddress)
+print (addr)
+
+
 ## Results after executing R Script - 
 
 ##[1] "Call: 877-739-2990"
-##[1] "Rental Office Address:1605 Plaza DriveWoodbridge, NJ 07095"
 ##[1] "Office Hours: Daily 10-5Thursday 10-7"
+##[1] "Rental Office Address:1605 Plaza DriveWoodbridge, NJ 07095"
+
 
 
